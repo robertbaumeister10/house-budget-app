71109d916ca19a5924b1adae17730c7f240aca73
Robert Baumeister <berlin_gerrit@hotmail.com>
2026-02-28 18:43:21 +0100

    xx lokal privatekey

--- src/ethereum.js
+++ src/ethereum.js
@@ -3,10 +3,9 @@ import { ethers } from "ethers";
 import TestContractJSON from "./TestContract.json";
 
 const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const PRIVATE_KEY = "xxx";
 
 export function getContract() {
-  // v6: kein "providers." mehr!
   console.log("Try to getContract!");
   const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
   const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
@@ -15,7 +14,7 @@ export function getContract() {
 
 export async function pingTest() {
     console.log("try to call pingTest!");
-  const contract = getContract();
-  await contract.pingTest(); // view-call, kein tx.wait()
-  console.log("pingTest erfolgreich!");
+    const contract = getContract();
+    await contract.pingTest();
+    console.log("pingTest erfolgreich!");
 }
