420c3c4999a11f14744df80aed5bfc49f774e659
GerritRichter <gerrit.richter@deutschebahn.com>
2026-03-18 15:43:37 +0100

    Add chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,77 @@
 import { ping, whitelistAddress } from "./ethereum";
+import { Button, HStack, Tabs } from "@chakra-ui/react";
+import { For, SimpleGrid} from "@chakra-ui/react"
+import { LuFolder, LuSquareCheck, LuUser } from "react-icons/lu"
 
 function App() {
 
+  const executeContractAction = async ({
+    startMessage,
+    successMessage,
+    errorMessage,
+    action,
+  }) => {
+    console.log(startMessage);
+    try {
+      await action();
+      console.log(successMessage);
+    } catch {
+      console.log(errorMessage);
+    }
+  };
+
   const callContract = async () => {
-    console.log("Try to call contract!");
-    try{
-      await ping();
-      console.log("Contract called!");
-    }
-    catch{
-      console.log("Contract could net be called!");
-    }
-  }
+    await executeContractAction({
+      startMessage: "Try to call contract!",
+      successMessage: "Contract called!",
+      errorMessage: "Contract could net be called!",
+      action: ping,
+    });
+  };
 
   const callWhitelistAddress = async (address) => {
-    console.log("Try to whitelist address...");
-    try{
-      await whitelistAddress(address);
-      console.log("Address is whitelisted!");
-    }
-    catch{
-      console.log("Address could not be whitelisted.");
-    }         
-  }
+    await executeContractAction({
+      startMessage: "Try to whitelist address...",
+      successMessage: "Address is whitelisted!",
+      errorMessage: "Address could not be whitelisted.",
+      action: () => whitelistAddress(address),
+    });
+  };
 
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <div><button onClick={callContract}>Call Contract!</button></div>
       <button onClick={() => callWhitelistAddress("0xh123")}>Whitelist Address</button>
+      <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+     <SimpleGrid columns={2} gap="14" width="full">
+          <Tabs.Root  defaultValue="members" >
+            <Tabs.List>
+              <Tabs.Trigger value="members">
+                <LuUser />
+                Members
+              </Tabs.Trigger>
+              <Tabs.Trigger value="projects">
+                <LuFolder />
+                Projects
+              </Tabs.Trigger>
+              <Tabs.Trigger value="tasks">
+                <LuSquareCheck />
+                Settings
+              </Tabs.Trigger>
+            </Tabs.List>
+            <Tabs.Content value="members">
+              Manage your team members
+            </Tabs.Content>
+            <Tabs.Content value="projects">Manage your projects</Tabs.Content>
+            <Tabs.Content value="tasks">
+              Manage your tasks for freelancers
+            </Tabs.Content>
+          </Tabs.Root>
+    </SimpleGrid>
     </div>
   );
 }
